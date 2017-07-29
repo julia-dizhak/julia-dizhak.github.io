6e853126b35d17aae3d45cac412e680f320c8471
Yulia <yulia.scherbina@gmail.com>
2017-07-29 18:59:53 +0200

    ideas of private page on github

---
+++ static/js/foxes.js
@@ -0,0 +1,11 @@
+// Draw as many fox as you want!
+
+var fox = document.getElementById("fox");
+
+var drawFoxes = function (howManyTimes) {
+  for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + " =^.^= " + "Hello");
+  }
+}
+
+drawFoxes(1) // You can put any number here instead of 10.

0982690e6c8c8bb33fa982b4a4ef4297887a8142
Yulia <yulia.scherbina@gmail.com>
2017-08-13 21:02:41 +0200

    doing arithmetic with functions

---
+++ static/js/functions.js
@@ -0,0 +1,16 @@
+var double = function(number) {
+  return number * 2;
+}
+double(3);
+
+var add = function(a,b) {
+  return a + b;
+}
+
+var multiply = function(a,b) {
+  return a * b;
+}
+
+var arithmetic = add(multiply(36325, 9824), 777);
+
+console.log(arithmetic);

2b8760717fd632bf2de141962d72bfbb1ee0b616
Yulia <yulia.scherbina@gmail.com>
2017-08-13 21:36:57 +0200

    are these arrays the same

--- static/js/about.js
+++ static/js/about.js
@@ -3,9 +3,23 @@ var pickRandomWord = function(words) {
   return words[Math.floor(Math.random() * words.length)];
 };
 
+// a function to check if arrays are the same
+var areArraysSame = function(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 var randomAdjectives = ["young", "professional", "sports", "pretty"],
     adjectiveElement = document.getElementById("adjective"),
-    oneRandomAdjective = pickRandomWord(randomAdjectives),
 
     randomWhatILoves = [
       "dogs",
@@ -32,7 +46,14 @@ var randomAdjectives = ["young", "professional", "sports", "pretty"],
     whereILiveElement = document.getElementById("live"),
     oneRandomWhereILive = pickRandomWord(randomPlaces);
 
+if ( areArraysSame(randomAdjectives, randomWhatILoves) != true  ) {
+  var oneRandomAdjective = pickRandomWord(randomAdjectives);
+  console.log(oneRandomAdjective);
+}
+
 // Output in DOM element
 adjectiveElement.innerHTML = oneRandomAdjective;
 whatILoveElement.innerHTML = oneRandomWhatILove;
 whereILiveElement.innerHTML = oneRandomWhereILive;
+
+debugger;
